531a184f846b4403fa91073315cebb45cf8ec9c0
jun.sun <jun.sun@media-smart.cn>
2020-03-26 15:16:20 +0800

    update fc_comverter

--- vedastr/converter/fc_converter.py
+++ vedastr/converter/fc_converter.py
@@ -3,24 +3,19 @@ import torch
 from .registry import CONVERTERS
 from .base_convert import BaseConverter
 
+
 @CONVERTERS.register_module
 class FCConverter(BaseConverter):
-    """ Convert between text-label and text-index """
 
-    def __init__(self, character):
-        # character (str): set of the possible characters.
-        # [s] for end-of-sentence token.
+    def __init__(self, character, batch_max_length=25):
+
         list_token = ['[s]']
         ignore_token = ['[ignore]']
         list_character = list(character)
-        self.character = list_token + list_character + ignore_token
+        super(FCConverter, self).__init__(character=list_token + list_character + ignore_token,
+                                          batch_max_length=batch_max_length)
         self.ignore_index = len(self.character) - 1
-        self.num_class = len(self.character) - 1
-
-        self.dict = {}
-        for i, char in enumerate(self.character):
-            # print(i, char)
-            self.dict[char] = i
+        self.dict.pop(ignore_token[0])
 
     def encode(self, text, batch_max_length=25):
         length = [len(s) + 1 for s in text]  # +1 for [s] at end of sentence.
